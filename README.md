1d9b8f71637005020619f878a8ed59cb7b56a872
35p10 <lucca360@outlook.com>
2023-07-21 23:36:57 -0500

    feat: process video at a specific frame rate

--- README.md
+++ README.md
@@ -4,7 +4,13 @@
 
 The input to the program is the path to the video as a command-line argument:
 ```
-./videoToFrames path/to/video.mp4
+videoToFrames path/to/video.mp4
+```
+
+If you want to process the video at a specific frame rate, you can provide the desired frames per second as the second argument.
+
+```
+videoToFrames path/to/video.mp4 5
 ```
 
 ## Output
--- main.cpp
+++ main.cpp
@@ -6,12 +6,17 @@
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " path/to/video.mp4" << std::endl;
+    if (argc < 2 || argc > 3) {
+        std::cerr << "Usage: " << argv[0] << " path/to/video.mp4 [fps]" << std::endl;
         return 1;
     }
 
     std::string videoPath = argv[1];
+    double desiredFPS = -1.0; // Default value: use all frames
+
+    if (argc == 3) {
+        desiredFPS = std::stod(argv[2]);
+    }
 
     cv::VideoCapture videoCapture(videoPath);
     if (!videoCapture.isOpened()) {
@@ -19,10 +24,31 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    // get information of the video
+    int totalFrames = static_cast<int>(videoCapture.get(cv::CAP_PROP_FRAME_COUNT));
+    double frameRate = videoCapture.get(cv::CAP_PROP_FPS);
+    double duration = totalFrames / frameRate;
+    cv::Size resolution(static_cast<int>(videoCapture.get(cv::CAP_PROP_FRAME_WIDTH)), static_cast<int>(videoCapture.get(cv::CAP_PROP_FRAME_HEIGHT)));
+    std::cout << "============================" << std::endl;
+    std::cout << "VIDEO INFORMATION" << std::endl;
+    std::cout << "============================" << std::endl;
+    std::cout << "Total Frames: " << totalFrames << std::endl;
+    std::cout << "Frame Rate: " << frameRate << " FPS" << std::endl;
+    std::cout << "Duration: " << duration << " seconds" << std::endl;
+    std::cout << "Resolution: " << resolution.width << "x" << resolution.height << std::endl;
+    std::cout << "============================" << std::endl;
+
     fs::path videoDir = fs::path(videoPath).stem();
-    fs::create_directory(videoDir); 
+    fs::create_directory(videoDir);
 
     int frameNumber = 0;
+    int framesSaved = 0;
+    int frameSkip = 1;
+    if (desiredFPS > 0.0) {
+        frameSkip = static_cast<int>(videoCapture.get(cv::CAP_PROP_FPS) / desiredFPS);
+        if (frameSkip <= 0) frameSkip = 1;
+    }
+
     while (true) {
         cv::Mat frame;
         videoCapture >> frame;
@@ -30,14 +56,17 @@ int main(int argc, char** argv) {
         if (frame.empty())
             break;
 
-        std::string imageName = videoDir.string() + "/frame_" + std::to_string(frameNumber) + ".jpg";
-        cv::imwrite(imageName, frame);
+        if (frameNumber % frameSkip == 0) {
+            std::string imageName = videoDir.string() + "/frame_" + std::to_string(frameNumber) + ".jpg";
+            cv::imwrite(imageName, frame);
+            framesSaved++;
+        }
 
         frameNumber++;
     }
 
     videoCapture.release();
-    std::cout << "Saved " << frameNumber << " frames as images in the folder \"" << videoDir.string() << "\"" << std::endl;
+    std::cout << "Saved " << framesSaved << " frames as images in the folder \"" << videoDir.string() << "\"" << std::endl;
 
     return 0;
 }
